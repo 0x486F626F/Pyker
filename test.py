43c51cae993575ca409d302e3d1dab72049921e7
Hongbo Zhang <hongbozhang.ca@gmail.com>
2020-04-15 02:23:59 -0400

    pboai

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 import game
 import player
+import pboai
 
 p1 = player.Player(100)
-p2 = player.Player(75)
-p3 = player.Player(50)
-g = game.Game(1, [100, 75, 50], [p1, p2, p3])
+p2 = pboai.Player(100)
+g = game.Game(1, [100, 100], [p1, p2])
 
 g.start()
