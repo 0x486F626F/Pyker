5a2a57b221bbf2fa128904a8aecd30ab66dd5c08
Hongbo Zhang <hongbozhang.ca@gmail.com>
2020-04-15 01:35:40 -0400

    comp all pairs

--- pbo/test.py
+++ pbo/test.py
@@ -1,8 +1,37 @@
 import handrank
+import threading
 
 
 h = handrank.Handrank()
-p = h.possible_hands([[1, 10], [1, 9], [1, 11]], [[1, 0], [1, 12]])
-print(len(p))
-for i in range(5):
-    print(p[i][0], h.decode(p[i][1]), p[i][2])
+
+result = [[[] for i in range(52)] for j in range(52)]
+
+
+def worker(i,j):
+    ci = [i//13,i%13]
+    cj = [j//13,j%13]
+    result[i][j] = result[j][i] = h.eval([ci,cj],[],50)
+    print(ci, cj, result[i][j])
+
+threads = []
+
+for i in range(52):
+    for j in range(i+1,52):
+        threads.append(threading.Thread(target=worker, args=(i,j,)))
+
+print(len(threads))
+w = 2
+
+for i in range(0, len(threads), w):
+    for t in range(w):
+        threads[i+t].start()
+    for t in range(w):
+        threads[i+t].join()
+        print(i+t)
+
+f = open('start.txt', 'w')
+
+for i in range(52):
+    for j in range(i+1,52):
+        r = result[i][j]
+        f.write('%d %d %d %d %f %f %f %f\n' % (i//13,i%13,j//13,j%13, r[0], r[1], r[2], r[3]))
