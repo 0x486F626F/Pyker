3b8618e654eb5cc62b16153dbbde19c7e8b60749
Hongbo Zhang <hongbozhang.ca@gmail.com>
2020-04-15 23:01:52 -0400

    fix bug in hand.py; cross-validated with cpp version

--- cpp/hand.h
+++ cpp/hand.h
@@ -5,11 +5,13 @@
 int strength_high_cards(t_card cards);
 int strength_pair(t_card cards);
 int strength_two_pairs(t_card cards);
-
-int strength_royal_flush(t_card cards);     //strength range [0, 0]
-int strength_straight_flush(t_card cards);  //strength range [1, 10]
-int strength_four_of_a_kind(t_card cards);  //strength range [0, 168]
+int strength_three_of_a_kind(t_card cards);  
+int strength_straight(t_card cards);           //strength range [0, 10]
+int strength_flush(t_card cards);
 int strength_full_house(t_card cards);      //strength range [1, 167]
-int strength_flush(t_card cards);           //strength range [0, 10]
+int strength_four_of_a_kind(t_card cards);  //strength range [0, 168]
+int strength_straight_flush(t_card cards);  //strength range [1, 10]
+int strength_royal_flush(t_card cards);     //strength range [0, 0]
+int hand_strength(t_card cards);
 
 #endif
