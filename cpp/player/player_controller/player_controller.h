f848af0fa5236f23d17b6cc2e0414da193be17fc
Arman Mielke <arman.mielke@yahoo.de>
2020-04-25 22:17:45 -0400

    Migrate PlayerController to C++

---
+++ cpp/player/player_controller/player_controller.h
@@ -0,0 +1,23 @@
+#ifndef PYKER_PLAYER_CONTROLLER_H
+#define PYKER_PLAYER_CONTROLLER_H
+
+
+#include "card.h"
+
+
+typedef std::tuple<int, int> BetRange;
+
+/** Betting this number of chips is will make the character fold. */
+const int FOLD = -1;
+
+
+/** An interface for objects that control the behaviour of a player. */
+class PlayerController {
+public:
+    /** @return the amount of chips that the player bets or FOLD, if the player folds. */
+    // TODO add trace parameter
+    virtual int bet(std::vector<Card> hand, BetRange bet_range) = 0;
+};
+
+
+#endif //PYKER_PLAYER_CONTROLLER_H
