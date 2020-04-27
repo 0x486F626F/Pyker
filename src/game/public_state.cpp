ac6e6cbca26d912bca2c11bcf079adb07ec2a708
Arman Mielke <arman.mielke@yahoo.de>
2020-04-27 02:28:32 -0400

    Add player-specific information to PublicState

--- src/game/public_state.cpp
+++ src/game/public_state.cpp
@@ -1,1 +1,44 @@
 #include "public_state.h"
+
+
+template <typename T>
+/// @return the elements of vec at the specified indices
+std::vector<T> vector_subset(const std::vector<T>& vec, const std::vector<size_t>& indices) {
+    std::vector<T> subset;
+    for (size_t index : indices) {
+        subset.push_back(vec[index]);
+    }
+    return subset;
+}
+
+
+void PublicState::start_game() {
+    bets = std::vector(balances.size(), 0);
+    folded = std::vector(balances.size(), false);
+    public_cards.clear();
+    // TODO move dealer_index
+}
+
+std::vector<size_t> PublicState::remaining_player_indices() const {
+    std::vector<size_t> indices;
+
+    for (int i = 0; i < balances.size(); i++) {
+        if (balances[i] > 0 && bets[i] > 0) {
+            indices.push_back(i);
+        }
+    }
+
+    return indices;
+}
+
+std::vector<int> PublicState::remaining_balances() const {
+    return vector_subset(balances, remaining_player_indices());
+}
+
+std::vector<int> PublicState::remaining_bets() const {
+    return vector_subset(bets, remaining_player_indices());
+}
+
+std::vector<bool> PublicState::remaining_folded() const {
+    return vector_subset(folded, remaining_player_indices());
+}
--- src/game/public_state.h
+++ src/game/public_state.h
@@ -2,14 +2,39 @@
 #define PYKER_PUBLIC_STATE_H
 
 
-#include <cstddef>
+#include "cards/card.h"
 
 
+/// Public information about the game and the players.
 struct PublicState {
     int big_blind;
     int small_blind;
+    /// The amount of chips each of the players has left.
+    /// Use remaining_balances() for the balances of non-bankrupt players.
+    std::vector<int> balances;
+    /// The amount of chips bet by each player in the current game.
+    /// Same size as balances.
+    /// Use remaining_bets() for the bets of non-bankrupt players.
+    std::vector<int> bets;
+    /// For each player either true, if they have folded this game, or false, otherwise.
+    /// Same size as balances.
+    /// Use remaining_folded() to get this information only for non-bankrupt players.
+    std::vector<bool> folded;
+    std::vector<Card> public_cards;
     size_t dealer_index;
-    // TODO history, pot, balances, public cards, which players have folded, ...
+    // TODO history of betting actions
+
+    /// Resets bets, folded and public_cards. Moves dealer_index to the next non-bankrupt player.
+    void start_game();
+    /// @return the indices of all players that aren't bankrupt.
+    ///         A player is considered bankrupt if their balance is 0 and they haven't bet any chips this game.
+    [[nodiscard]] std::vector<size_t> remaining_player_indices() const;
+    /// @return balances of the non-bankrupt players.
+    [[nodiscard]] std::vector<int> remaining_balances() const;
+    /// @return the amount of chips bet in the current game by each non-bankrupt player.
+    [[nodiscard]] std::vector<int> remaining_bets() const;
+    /// @return for each non-bankrupt player: either true, if they have folded this game, or false, otherwise.
+    [[nodiscard]] std::vector<bool> remaining_folded() const;
 };
 
 
